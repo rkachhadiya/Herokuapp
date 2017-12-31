500d9419dcc6790388d77e496c92731cc77d8b1c
rkachhadiya <rajukachhadiya@gmail.com>
2018-01-01 02:24:37 +0530

    Remove testNG-6.11 Added testNG-6.12 Updated java class

--- Herokuapp/src/Herokuapp/TestCases/HomePageLinks.java
+++ Herokuapp/src/Herokuapp/TestCases/HomePageLinks.java
@@ -1,11 +1,17 @@
 package Herokuapp.TestCases;
 
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
 public class HomePageLinks {
 
 	@BeforeClass
 	private void launchDriver() {
-		
+		System.out.println("This is an issue");
+	}
+	
+	@Test
+	private void testMethod() {
+		System.out.println("This a test method!");
 	}
 }
