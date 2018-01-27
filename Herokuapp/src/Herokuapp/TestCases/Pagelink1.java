119832b50399afc1c270b5604ba1f485ffacc64e
rkachhadiya <rajukachhadiya@gmail.com>
2018-01-27 10:01:49 +0530

    Created and updated some initial files

--- Herokuapp/src/Herokuapp/TestCases/Pagelink1.java
+++ Herokuapp/src/Herokuapp/TestCases/Pagelink1.java
@@ -3,18 +3,16 @@ package Herokuapp.TestCases;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class HomePageLinks {
-
+public class Pagelink1 {
 	@BeforeClass
 	private void launchDriver() throws InterruptedException {
-		System.out.println("This is an issue");
+		System.out.println("Class 2");
 		Thread.sleep(1000);
 	}
 	
 	@Test
 	private void testMethod() throws InterruptedException {
 		System.out.println("This a test method!");
-		System.out.println(10/0);
-		Thread.sleep(1000);
+		Thread.sleep(2000);
 	}
 }
