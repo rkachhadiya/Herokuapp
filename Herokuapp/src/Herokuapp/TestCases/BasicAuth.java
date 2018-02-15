a09e4b6c2ddc914989b0e8db59de583534666627
rkachhadiya <rajukachhadiya@gmail.com>
2018-02-15 23:38:08 +0530

    Added two new class Updated one class

--- Herokuapp/src/Herokuapp/TestCases/BasicAuth.java
+++ Herokuapp/src/Herokuapp/TestCases/BasicAuth.java
@@ -1,12 +1,18 @@
 package Herokuapp.TestCases;
 
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
 public class BasicAuth {
 	
 	@BeforeClass
 	public void setUp(){
 		System.out.println("This is a success message");
+	}
+	
+	@Test
+	public void testMethod(){
+		System.out.print("Test method run...");
 	}
 
 }
---
+++ Herokuapp/src/Herokuapp/TestCases/TestClasss.java
@@ -0,0 +1,8 @@
+package Herokuapp.TestCases;
+
+public class TestClasss {
+
+	public void method(){
+	System.out.println("testing");
+	}
+}
