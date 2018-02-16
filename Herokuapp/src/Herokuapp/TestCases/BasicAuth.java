5e5a4b3a8b8d088a8594200de92383145667b807
RKSony <RKSony@RKSony-PC>
2018-02-16 23:26:09 +0530

    Handle authentication dialog in selenium webdriver.

--- Herokuapp/src/Herokuapp/TestCases/BasicAuth.java
+++ Herokuapp/src/Herokuapp/TestCases/BasicAuth.java
@@ -1,18 +1,33 @@
 package Herokuapp.TestCases;
 
+import java.io.FileNotFoundException;
+
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class BasicAuth {
+import Herokuapp.Base.DriverClass;
+
+public class BasicAuth extends DriverClass{
+	
+	DriverClass driverclass;
 	
 	@BeforeClass
-	public void setUp(){
-		System.out.println("This is a success message");
+	public void setUp_Driver() throws FileNotFoundException{
+		setUp();
 	}
 	
 	@Test
 	public void testMethod(){
-		System.out.print("Test method run...");
+		String user = "admin", password = "admin";
+		String URL = "http://" + user + ":" + password + "@" + "the-internet.herokuapp.com/basic_auth";
+		
+		driver.get(URL + "/");		
+	}
+	
+	@AfterClass
+	private void tearDown(){
+		driver.quit();
 	}
 
 }
